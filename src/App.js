87d327969c7eb0bc2a30c22e3cd3f06c6edef78f
John N. Norman <normanj777@gmail.com>
2021-03-30 15:31:10 -0400

    customize app remove starter files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      10,000+ of our users love our products.
+
+      We only provide great products combined with excellent customer service.
+      See what our satisfied customers are saying about our services.
+
+      Rated 5 Stars in Reviews
+      Rated 5 Stars in Report Guru
+      Rated 5 Stars in BestTech
+
+      Colton Smith
+      Verified Buyer
+      "We needed the same printed design as the one we had ordered a week prior.
+      Not only did they find the original order, but we also received it in time.
+      Excellent!"
+
+      Irene Roberts
+      Verified Buyer
+      "Customer service is always excellent and very quick turn around. Completely
+      delighted with the simplicity of the purchase and the speed of delivery."
+
+      Anne Wallace
+      Verified Buyer
+      "Put an order with this company and can only praise them for the very high
+      standard. Will definitely use them again and recommend them to everyone!"
+
+      <div class="attribution">
+        Challenge by
+  <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer"
+        >Frontend Mentor</a
+        >. Coded by <a href="https://github.com/norman02">John Norman</a>.
+</div>
     </div>
   );
 }
